a95a79a7526bf7b537db12a7870ad011be02e8af
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-04-16 14:57:49 +0200

    updated skills json file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import IntroSection from "./sections/IntroSection"
 import TechSection from "./sections/TechSection"
 import ProjectsSection from "./sections/ProjectsSection"
 import ContactSection from "./sections/ContactSection"
+
 import tech from "./data/tech.json"
 import projects from "./data/projects.json"
 
